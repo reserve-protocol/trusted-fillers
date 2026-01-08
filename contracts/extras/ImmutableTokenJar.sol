864229d67bd1978356b8a866ea134c4d692af3cc
Akshat Mittal <itsreallyakshat@gmail.com>
2026-01-09 03:33:00 +0530

    Add initial draft

---
+++ contracts/extras/ImmutableTokenJar.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import { SafeERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import { GPv2OrderLib } from "@src/fillers/cowswap/GPv2OrderLib.sol";
+import { GPV2_SETTLEMENT, GPV2_VAULT_RELAYER, D27 } from "@src/fillers/cowswap/Constants.sol";
+
+contract ImmutableTokenJar {
+    using GPv2OrderLib for GPv2OrderLib.Data;
+    using SafeERC20 for IERC20;
+
+    address public immutable destination;
+    IERC20 public immutable token;
+
+    error ImmutableTokenJar__OrderCheckFailed(uint256 errorCode);
+
+    constructor(address _destination, IERC20 _token) {
+        destination = _destination;
+        token = _token;
+    }
+
+    /// @dev preHook for CowSwap Order
+    function approveTokensToRelayer(IERC20[] memory _tokens) external {
+        for (uint256 i = 0; i < _tokens.length; i++) {
+            _tokens[i].forceApprove(GPV2_VAULT_RELAYER, type(uint256).max);
+        }
+    }
+
+    /// @dev Transfers all held `token` to `destination`
+    function pushTokens() external {
+        uint256 balance = token.balanceOf(address(this));
+
+        token.safeTransfer(destination, balance);
+    }
+
+    /// @dev Validates CowSwap order for a fill via EIP-1271
+    function isValidSignature(bytes32 orderHash, bytes calldata signature) external view returns (bytes4) {
+        // Decode signature to get the CowSwap order
+        GPv2OrderLib.Data memory order = abi.decode(signature, (GPv2OrderLib.Data));
+
+        // Verify Order Hash
+        require(orderHash == order.hash(GPV2_SETTLEMENT.domainSeparator()), ImmutableTokenJar__OrderCheckFailed(0)); // Invalid Order Hash
+
+        require(order.buyToken == token, ImmutableTokenJar__OrderCheckFailed(2)); // Invalid Buy Token
+        require(order.feeAmount == 0, ImmutableTokenJar__OrderCheckFailed(3)); // Must be a Limit Order
+        require(order.receiver == address(this), ImmutableTokenJar__OrderCheckFailed(4)); // Receiver must be self
+        require(order.sellTokenBalance == GPv2OrderLib.BALANCE_ERC20, ImmutableTokenJar__OrderCheckFailed(5)); // Must use ERC20 Balance
+        require(order.buyTokenBalance == GPv2OrderLib.BALANCE_ERC20, ImmutableTokenJar__OrderCheckFailed(6)); // Must use ERC20 Balance
+        require(order.sellAmount != 0, ImmutableTokenJar__OrderCheckFailed(7));
+        require(order.buyAmount != 0, ImmutableTokenJar__OrderCheckFailed(8));
+
+        // If all checks pass, return the magic value
+        return this.isValidSignature.selector;
+    }
+}
